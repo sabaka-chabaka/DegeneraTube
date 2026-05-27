176047f92de8259c8654388d746acb52db93de99
sabaka-chabaka <gamekimg360@gmail.com>
2026-05-27 11:49:38 +0400

    baserepo

--- backend/DegeneraTube.Infrastructure/Repositories/IRepository.cs
+++ backend/DegeneraTube.Infrastructure/Repositories/IRepository.cs
@@ -6,7 +6,7 @@ public interface IRepository<T> where T : BaseEntity
 {
     Task<T?> GetByIdAsync(Guid id, CancellationToken ct = default);
     Task<List<T>> GetAllAsync(CancellationToken ct = default);
-    Task<T> AddAsync(T entity, CancellationToken ct = default);
+    Task AddAsync(T entity, CancellationToken ct = default);
     void Update(T entity);
     void Remove(T entity);
     Task SaveAsync(CancellationToken ct = default);
