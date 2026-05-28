5ef9d86b8e6b2add82b24ef35e28c4e44a6afc97
sabaka-chabaka <gamekimg360@gmail.com>
2026-05-28 12:42:14 +0400

    YAAAYU

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,1 +1,19 @@
 import './style.css'
+import { route, initRouter } from './router';
+import { renderNavbar } from './components/Navbar';
+import { HomePage } from './pages/HomePage';
+import { VideoPage } from './pages/VideoPage';
+import { LoginPage, RegisterPage } from './pages/LoginPage';
+import { UploadPage } from './pages/UploadPage';
+import { ProfilePage } from './pages/ProfilePage';
+
+renderNavbar();
+
+route('/', () => HomePage());
+route('/video/:id', (p) => VideoPage(p));
+route('/login', () => Promise.resolve(LoginPage()));
+route('/register', () => Promise.resolve(RegisterPage()));
+route('/upload', () => Promise.resolve(UploadPage()));
+route('/profile/:id', (p) => ProfilePage(p));
+
+initRouter();
